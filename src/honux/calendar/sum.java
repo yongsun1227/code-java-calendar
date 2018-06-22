bdb0d9174103d5d862c27b6bd91cd1f703577980
502-13\001 <yongsun1227@icloud.com>
2018-06-22 11:17:00 +0900

    new

--- src/honux/calendar/sum.java
+++ src/honux/calendar/sum.java
@@ -15,7 +15,18 @@ public class sum {
 
 		System.out.println("두 수의 합은 "+sum1+"입니다");
 		
-		
 	}
-
 }
+		/*int a,b;
+		System.out.println("두 수를 입력해주세요");
+		Scanner scanner = new Scanner(System.in);키보드입력을 받는다
+		String s1, s2;
+		s1 = scanner.next();
+		s2 = scanner.next();
+		
+		System.out.printf("두 수의 합은 %d입니다.", a+b);
+		scanner.close();
+		
+
+*/
+
