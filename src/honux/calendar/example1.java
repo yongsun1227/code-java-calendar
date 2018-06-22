24cd58664cc86b82fd81a48fe7317e5470b63949
502-13\001 <yongsun1227@icloud.com>
2018-06-22 12:23:28 +0900

    max days of the month

--- src/honux/calendar/example1.java
+++ src/honux/calendar/example1.java
@@ -1,23 +1,33 @@
 package honux.calendar;
+
 import java.util.*;
 
 public class example1 {
 
 	public static void main(String[] args) {
 		int a;
+		
 		System.out.println("달을 입력하세요");
 		Scanner scanner = new Scanner(System.in);
 		String s1 = scanner.next();
 		a = Integer.parseInt(s1);
-		
-		if(a == 2) {
+
+		if (a == 2) {
 			System.out.println("2월은 28일까지 있습니다.");
-		}else if(a%2 == 0) {
-			System.out.println(a+"월은 30일까지 있습니다.");
-		}else {
-			System.out.println(a+"월은 31일까지 있습니다.");
+		} else if (a % 2 == 0) {
+			System.out.println(a + "월은 30일까지 있습니다.");
+		} else {
+			System.out.println(a + "월은 31일까지 있습니다.");
 		}
+		scanner.close();
 
 	}
+	
+//int month = scanner.nextInt();
+//	int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
+//	System.out.printf("%월은 %일까지 있습니다.\n", month, maxDays[month - 1]);
+//	scanner.close();
+	
+	
 
 }
