732ba4ca86ff047459e02aeb89b90010aa18762b
502-13\001 <yongsun1227@icloud.com>
2018-06-22 12:14:24 +0900

    월의 최대일수

---
+++ src/honux/calendar/example1.java
@@ -0,0 +1,23 @@
+package honux.calendar;
+import java.util.*;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		int a;
+		System.out.println("달을 입력하세요");
+		Scanner scanner = new Scanner(System.in);
+		String s1 = scanner.next();
+		a = Integer.parseInt(s1);
+		
+		if(a == 2) {
+			System.out.println("2월은 28일까지 있습니다.");
+		}else if(a%2 == 0) {
+			System.out.println(a+"월은 30일까지 있습니다.");
+		}else {
+			System.out.println(a+"월은 31일까지 있습니다.");
+		}
+
+	}
+
+}
